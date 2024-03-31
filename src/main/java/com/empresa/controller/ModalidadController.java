58360b506cf92e5a0846b338c7d1417f07816838
andrescabanillas13 <andrescabanillas13@gmail.com>
2024-03-31 10:15:33 -0500

    cambios 2

--- src/main/java/com/empresa/controller/ModalidadController.java
+++ src/main/java/com/empresa/controller/ModalidadController.java
@@ -1,11 +1,13 @@
 package com.empresa.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,15 +25,20 @@ public class ModalidadController {
 	
 	@GetMapping
 	public List<Modalidad> listaModalidad(){
-		List<Modalidad> lstSalida = service.listaModalidad();
+		List<Modalidad> lstSalida = service.listaModalidad();	
 		return lstSalida;
 	}
 	
-	public ResponseEntity<?> insertaModalidad(@RequestBody Modalidad obj){
+	@PostMapping
+	public ResponseEntity<?>  insertaModalidad(@RequestBody Modalidad obj){
 		List<String> lstSalida = new ArrayList<String>();
 		
+		obj.setEstado(1);
+		obj.setFechaRegistro(new Date());
+		obj.setFechaActualizacion(new Date());
+		
 		Modalidad objSalida = service.insertaModalidad(obj);
-		if(objSalida == null) {
+		if (objSalida == null) {
 			lstSalida.add("Error en el registro");
 		}else {
 			lstSalida.add("se registra la modalidad con el id : " + objSalida.getIdModalidad());
